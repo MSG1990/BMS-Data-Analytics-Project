20e840ec05d59c7c890bef2da2f14a903a9850f3
Mohamed Gabir <100272541+MSG1990@users.noreply.github.com>
2026-01-13 08:23:26 +0300

    Add files via upload

---
+++ Chillers Run status - bz to sv.sql
@@ -0,0 +1,108 @@
+-- this script is to import chiller_Run_status data then
+-- cleaning it and transfer it to silver layer
+
+use bronze;
+
+-- creating the Chillers_run_status table
+-- datetime info must be imported as a string then converted to datetime
+create table `BZ_CHILLERS_RUN_STATUS`(
+`date_time` varchar (20),
+`chiller 02 run status`varchar (20),
+`Dt2` varchar (20),
+`chiller 01 run status`varchar (20),
+`Dt3` varchar (20),
+`chiller 03 run status`varchar (20),
+`Dt4` varchar (20),
+`chiller 04 run status`varchar (20)
+);
+
+
+set global local_infile = 1;
+show variables like 'local_infile';
+
+load data local infile '/Users/basimyousef/Documents/BMS Data Analytics Project/BMS DATA - JAN 12/CHILLER 2 RUN_2025-12-12_02-06-57_2026-01-09_02-06-57.csv'
+	into table bronze.BZ_CHILLERS_RUN_STATUS
+    FIELDS TERMINATED BY ','
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\r\n'
+    ignore 1 lines;
+    
+select * from BZ_CHILLERS_RUN_STATUS;
+
+-- deleting the repeated time stamps
+
+alter table bz_chillers_run_status
+drop column dt2,
+drop column dt3,
+drop column dt4;
+
+DESCRIBE bz_chillers_run_status;
+
+-- resetting date_ime to datetime data type
+-- 1. Disable safe mode for this session
+SET SQL_SAFE_UPDATES = 0;
+
+-- 2. Update only valid strings and turn empty ones into NULL
+UPDATE BZ_CHILLERS_RUN_STATUS 
+SET date_time = STR_TO_DATE(TRIM(`date_time`), '%Y-%m-%d %H:%i:%s')
+WHERE TRIM(`date_time`) != '' AND `date_time` IS NOT NULL;
+
+    
+ALTER TABLE BZ_CHILLERS_RUN_STATUS 
+MODIFY COLUMN `chiller 02 run status` TINYINT,
+MODIFY COLUMN `chiller 01 run status` TINYINT,
+MODIFY COLUMN `chiller 03 run status` TINYINT,
+MODIFY COLUMN `chiller 04 run status` TINYINT;
+    
+-- verification of the previous step
+describe bz_chillers_run_status;
+
+-- unpivoting the table and saving the work as a table in silver data base
+drop table if exists silver.sv_chillers_run_status;
+create table silver.sv_chillers_run_status as
+select
+	Date_time,
+	'Chiller 1' as Chiller_id,
+	`chiller 01 run status`  as RUN_STATUS
+from `bz_chillers_run_status`
+union all
+select
+	Date_time,
+    'Chiller 2' as Chiller_id,
+    `chiller 02 run status`  as RUN_STATUS
+from `bz_chillers_run_status`
+union all
+select
+	Date_time,
+    'Chiller 3' as Chiller_id,
+    `chiller 03 run status` as RUN_STATUS
+from `bz_chillers_run_status`
+union all
+select
+	Date_time,
+    'Chiller 4' as Chiller_id,
+    `chiller 04 run status`  as RUN_STATUS
+from `bz_chillers_run_status`
+;
+
+select * from silver.sv_chillers_run_status limit 1000 offset 3000;
+
+-- deleting nulls in time:
+
+DELETE FROM silver.sv_chillers_run_status 
+WHERE date_time IS NULL 
+   OR date_time = '' 
+   OR date_time = ' ';
+
+-- sensors's data time-sequencing & niose elimintation
+drop table if exists `gold`.`gd_chillers_run_status`;
+create table `gold`.`gd_chillers_run_status` as
+select
+    DATE_FORMAT(date_time, '%Y-%m-%d %H:%i:00') AS date_time,
+    chiller_id,
+    RUN_STATUS AS run_status
+from silver.sv_chillers_run_status
+;
+
+-- verification
+select * from gold.gd_chillers_run_status limit 1000 offset 4000;
---
+++ chillers rla.sql
@@ -0,0 +1,209 @@
+-- this script is to import chiller_RLA5 data then
+-- cleaning it and transfering it to the silver layer
+
+-- login into the boronze database
+select database();
+use bronze;
+
+-- creating the Chillers_RLA% table
+-- datetime info must be imported as a string then converted ti datetime
+create table if not exists bz_chillers_RLA (
+	date_time varchar(20),
+	CHILLER_2_SYS1_RLA varchar(20),
+    date_time_1 varchar(20),
+	CHILLER_1_SYS1_RLA varchar(20),
+    date_time_2 varchar(20),
+	CHILLER_1_SYS2_RLA varchar(20),
+    date_time_3 varchar(20),
+	CHILLER_2_SYS2_RLA varchar(20),
+    date_time_4 varchar(20),
+	CHILLER_3_SYS1_RLA varchar(20),
+    date_time_5 varchar(20),
+	CHILLER_3_SYS2_RLA varchar(20),
+    date_time_6 varchar(20),
+	CHILLER_4_SYS1_RLA varchar(20),
+    date_time_7 varchar(20),
+	CHILLER_4_SYS2_RLA varchar(20)
+    );
+    
+-- improrting the data bi wizard
+SET GLOBAL local_infile = 1;
+
+LOAD DATA LOCAL INFILE '/Users/basimyousef/Documents/BMS Data Analytics Project/BMS DATA - JAN 12/CHILLER 2 - SYS1 RLA %_2025-12-09_21-13-00_2026-01-06_21-13-00.csv'
+INTO TABLE bz_chillers_RLA
+FIELDS TERMINATED BY ',' 
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(date_time,
+	CHILLER_2_SYS1_RLA,
+    date_time_1,
+	CHILLER_1_SYS1_RLA,
+    date_time_2,
+	CHILLER_1_SYS2_RLA,
+    date_time_3,
+	CHILLER_2_SYS2_RLA,
+    date_time_4,
+	CHILLER_3_SYS1_RLA,
+    date_time_5,
+	CHILLER_3_SYS2_RLA,
+    date_time_6,
+	CHILLER_4_SYS1_RLA,
+    date_time_7,
+	CHILLER_4_SYS2_RLA);
+    
+-- verification
+select * from `bz_chillers_RLA` limit 1000 offset 2000;
+
+-- deleting unnecessary columns/data
+alter table `bz_chillers_RLA` 
+drop column date_time_1, 
+drop column date_time_2,
+drop column date_time_3,
+drop column date_time_4,
+drop column date_time_5,
+drop column date_time_6,
+drop column date_time_7;
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM bz_chillers_RLA WHERE date_time IS NULL OR date_time = '';
+
+-- fixting data type:
+
+CREATE TABLE silver.sv_chillers_RLA (
+    date_time DATETIME,
+    CHILLER_1_SYS1_RLA FLOAT,
+    CHILLER_1_SYS2_RLA FLOAT,
+    CHILLER_2_SYS1_RLA FLOAT,
+    CHILLER_2_SYS2_RLA FLOAT,
+    CHILLER_3_SYS1_RLA FLOAT,
+    CHILLER_3_SYS2_RLA FLOAT,
+    CHILLER_4_SYS1_RLA FLOAT,
+    CHILLER_4_SYS2_RLA FLOAT
+);
+
+INSERT INTO silver.sv_chillers_RLA
+SELECT 
+    STR_TO_DATE(trim(date_time), '%m/%d/%Y %H:%i:%s'),
+    CAST(`CHILLER_1_SYS1_RLA` AS FLOAT),
+    CAST(`CHILLER_1_SYS2_RLA` AS FLOAT),
+    CAST(`CHILLER_2_SYS1_RLA` AS FLOAT),
+    CAST(`CHILLER_2_SYS2_RLA` AS FLOAT),
+    CAST(`CHILLER_3_SYS1_RLA` AS FLOAT),
+    CAST(`CHILLER_3_SYS2_RLA` AS FLOAT),
+    CAST(`CHILLER_4_SYS1_RLA` AS FLOAT),
+    CAST(`CHILLER_4_SYS2_RLA` AS FLOAT)
+FROM bz_chillers_RLA;
+
+-- verification
+select * from silver.sv_chillers_RLA limit 1000;
+
+-- verification no null
+select * from silver.sv_chillers_RLA where date_time is null or 
+CHILLER_1_SYS1_RLA is null or
+CHILLER_1_SYS2_RLA is null or
+CHILLER_2_SYS1_RLA is null or
+CHILLER_2_SYS2_RLA is null or
+CHILLER_3_SYS1_RLA is null or
+CHILLER_3_SYS2_RLA is null or
+CHILLER_4_SYS1_RLA is null or
+CHILLER_4_SYS2_RLA is null;
+
+-- Unpoviting
+create table silver.sv_chillers_RLA_stacking (
+    date_time DATETIME,
+    chiller_name VARCHAR(50),
+    rla_value FLOAT
+);
+    
+INSERT INTO silver.sv_chillers_RLA_stacking (date_time, chiller_name, rla_value) -- Explicitly name the 3 columns
+	SELECT date_time, 'CHILLER 1_SYS1', CHILLER_1_SYS1_RLA FROM silver.sv_chillers_RLA
+	UNION ALL
+	SELECT date_time, 'CHILLER 1_SYS2', CHILLER_1_SYS2_RLA FROM silver.sv_chillers_RLA
+	UNION ALL
+	SELECT date_time, 'CHILLER 2_SYS1', CHILLER_2_SYS1_RLA FROM silver.sv_chillers_RLA
+	UNION ALL
+	SELECT date_time, 'CHILLER 2_SYS2', CHILLER_2_SYS2_RLA FROM silver.sv_chillers_RLA
+	UNION ALL
+	SELECT date_time, 'CHILLER 3_SYS1', CHILLER_3_SYS1_RLA FROM silver.sv_chillers_RLA
+	UNION ALL
+	SELECT date_time, 'CHILLER 3_SYS2', CHILLER_3_SYS2_RLA FROM silver.sv_chillers_RLA
+	UNION ALL
+	SELECT date_time, 'CHILLER 4_SYS1', CHILLER_4_SYS1_RLA FROM silver.sv_chillers_RLA
+	UNION ALL
+	SELECT date_time, 'CHILLER 4_SYS2', CHILLER_4_SYS2_RLA FROM silver.sv_chillers_RLA;
+    
+-- verification
+select * from silver.sv_chillers_RLA_stacking limit 1000;
+
+-- renaming RLA table in silver layer
+drop table if exists `silver`.`sv_chillers_RLA`;
+
+rename table `silver`.`sv_chillers_RLA_stacking` to `silver`.`sv_chillers_RLA`;
+
+-- splitting chiller name and sys:
+ALTER TABLE silver.sv_chillers_RLA 
+ADD COLUMN chiller_id VARCHAR(10),
+ADD COLUMN system_id VARCHAR(10);
+
+UPDATE silver.sv_chillers_RLA
+SET 
+    chiller_id = SUBSTRING_INDEX(chiller_name, '_', 1),  -- Gets 'CHILLER 1'
+    system_id = SUBSTRING_INDEX(chiller_name, '_', -1); -- Gets 'SYS1'
+    
+ALTER TABLE silver.sv_chillers_RLA 
+DROP COLUMN chiller_name;
+
+-- verification
+select * from silver.sv_chillers_RLA limit 1000;
+
+-- sensors's data time-sequencing & niose elimintation
+drop table if exists silver.sv_chillers_RLA_finetuned;
+create table silver.sv_chillers_RLA_finetuned as
+select
+    DATE_FORMAT(date_time, '%Y-%m-%d %H:%i:00') AS adjusted_date_time,
+    chiller_id,
+    system_id,
+    round(avg(rla_value),2) AS rla_value
+from silver.sv_chillers_RLA
+group by adjusted_date_time, chiller_id,system_id;
+
+-- verification
+select * from silver.sv_chillers_RLA_finetuned limit 1000;
+select * from silver.sv_chillers_RLA_finetuned limit 1000 offset 1000;
+select * from silver.sv_chillers_RLA_finetuned
+	order by adjusted_date_time asc, chiller_id asc;
+    
+-- capturing the RLA at the chiller level instead of system level
+drop table if exists silver.sv_chillers_RLA_finetuned_summedRLA;
+create table silver.sv_chillers_RLA_finetuned_summedRLA as
+select
+	adjusted_date_time,
+	chiller_id,
+	sum(rla_value) as total_rla
+from silver.sv_chillers_RLA_finetuned 
+group by adjusted_date_time, chiller_id;
+
+-- verification
+select * from silver.sv_chillers_RLA_finetuned_summedRLA limit 1000;
+
+-- to the gold schema
+drop table if exists gold.gd__chillers_RLA;
+create table gold.gd__chillers_RLA as 
+select
+	adjusted_date_time,
+	chiller_id,
+	total_rla
+from silver.sv_chillers_RLA_finetuned_summedRLA;
+
+-- maintaining standard naming convintion
+alter table gold.gd__chillers_RLA
+	rename column `adjusted_date_time` to `date_time`;
+    
+rename table gold.gd__chillers_RLA to gold.gd_chillers_RLA;
+
+-- verification
+select * from gold.gd_chillers_RLA 
+where chiller_id = 'chiller 2' or 'chiller 4';
+select * from gold.gd_chillers_RLA limit 1000 offset 2000;
+select * from gold.gd_chillers_RLA limit 1000 offset 4000;
